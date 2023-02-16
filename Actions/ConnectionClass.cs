f5db9911452408d012be6ed0887b51dd3d95132d
Rezonanzze <alexrezonanzze@gmail.com>
2023-02-16 17:22:25 +0300

    Созданы классы: Подключения и Навигации, Подключена БД

---
+++ Actions/ConnectionClass.cs
@@ -0,0 +1,16 @@
+﻿using BrovkiTest.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrovkiTest.Actions
+{
+    internal class ConnectionClass
+    {
+        // Подключение к БД
+        public static det_user7Entities ConnectionPoint { get; set; } = new det_user7Entities();
+
+    }
+}
---
+++ PageMain/AdminLoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrovkiTest.PageMain
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminLoginPage.xaml
+    /// </summary>
+    public partial class AdminLoginPage : Page
+    {
+        public AdminLoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
